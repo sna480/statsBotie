25ce874f82f3ae0cbe541afddac02c07c1959e9f
artur <arturstiv31@gmail.com>
2025-01-08 00:46:32 +0500

    commands fixed

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -10,9 +10,9 @@ cmd_router = Router()
 
 @cmd_router.message(CommandStart())
 async def start(message: types.Message):
-    await message.answer(get_str('texts/start.txt'), parse_mode=ParseMode.MARKDOWN, reply_markup=await menu_keyboard())
+    await message.answer(get_str('texts/commands/start.txt'), parse_mode=ParseMode.MARKDOWN, reply_markup=await menu_keyboard())
 
 
 @cmd_router.message(Command('about'))
 async def start(message: types.Message):
-    await message.answer(get_str('texts/about.txt'), parse_mode=ParseMode.MARKDOWN)
+    await message.answer(get_str('texts/commands/about.txt'), parse_mode=ParseMode.MARKDOWN)
